0d198ef9e6c3b9546aeac794e9e38262ef4ca434
onizar <nizaroumehdi@gmail.com>
2024-12-17 18:13:19 +0100

    importation de tailwind

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavbarComponent} from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    NavbarComponent
+  ],
+  standalone: true,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
