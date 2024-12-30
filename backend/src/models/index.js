a6cff80ab08efe8c72fb3dabcf1215b159b04112
norah_jane <norahbelinga@gmail.com>
2024-12-30 12:23:08 +0100

    no errors, swagger on productRoutes

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,6 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+export const sequelize = new Sequelize('postgres://username:password@localhost:5432/online_shop', {
+    dialect: 'postgres',
 });
